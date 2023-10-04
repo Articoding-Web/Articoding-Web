024b7779f3b7ebb62af0cdfc421dee1e80739532
nico <nico.mooser@gmail.com>
2023-10-04 19:23:48 +0200

    level loader

--- src/Phaser/phaserDemo.ts
+++ src/Phaser/phaserDemo.ts
@@ -1,16 +1,16 @@
 import * as Phaser from 'phaser';
-import LevelEditor from './LevelEditor/levelEditor';
+import LevelLoader from './LevelEditor/levelLoader';
 
 const config : Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     backgroundColor: '#16161D',
     // width: 800,
     // height: 600,
-    width: 14 * 70,
-    height: 14 * 50,
+    width: 980,
+    height: 700,
     zoom: 1,
     parent: 'phaserDiv',
-    scene: LevelEditor,
+    scene: LevelLoader,
 };
  
 
