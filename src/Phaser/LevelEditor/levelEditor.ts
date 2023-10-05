976358edc3eb7afa9f6f5a2df03505fdf5cd2d23
nico <nico.mooser@gmail.com>
2023-10-05 22:58:41 +0200

    Phaser.Actions.GridAlign()

--- src/Phaser/LevelEditor/levelEditor.ts
+++ src/Phaser/LevelEditor/levelEditor.ts
@@ -5,102 +5,134 @@ import customBlocks from './Blockly/customBlocks';
 import toolbox from './Blockly/toolbox';
 import { defineBlocks } from './Blockly/Block_Code';
 
-const TILE_WIDTH = 100;
-const TILE_HEIGHT = 100;
+const TILE_SIZE = 100;
 const INIT_TILES = 3;
+const TURRET_START_POS_X = 100;
+const TURRET_START_POS_Y = 100;
+
 export default class LevelEditor extends Phaser.Scene {
     workspace: Blockly.WorkspaceSvg;
     blocklyArea = <HTMLElement>document.getElementById('blocklyArea');
     blocklyDiv = <HTMLElement>document.getElementById('blocklyDiv');
-    dropZones: Phaser.GameObjects.Zone[][];
+    numTilesX: number;
+    numTilesY: number;
+    tiles: Phaser.GameObjects.Sprite[] = [];
+    turret: Phaser.GameObjects.Sprite;
 
     constructor() {
         super('LevelEditor');
     }
 
     preload(): void {
-        this.load.image('tileAsset', 'assets/Tiles/tile.png');
+        this.load.image('tile', 'assets/Tiles/tile.png');
         this.load.image('Turret', 'assets/LaserTurret.png');
     }
 
-    generateLevel(dimX: number, dimY: number, tileValue: number): number[][] {
-        //create 2d array to hold the level data
-        let level: number[][] = [];
-        for (let i = 0; i < dimX; i++) {
-            let row: number[] = [];
-            for (let j = 0; j < dimY; j++) {
-                row.push(tileValue);
+    generateLevel(): void {
+        console.debug("Generating level:");
+        console.debug(`numTiles: ${this.tiles.length}`);
+
+        // Varibles to center the tilemap
+        const SCREEN_X = this.cameras.main.width;
+        const SCREEN_Y = this.cameras.main.height;
+        let xCoord = (SCREEN_X - (this.numTilesX * TILE_SIZE)) / 2
+        let yCoord = (SCREEN_Y - (this.numTilesY * TILE_SIZE)) / 2
+
+        let newTotalNumTiles = this.numTilesX * this.numTilesY;
+        console.debug(`newTotalNumTiles: ${newTotalNumTiles}`);
+
+        if (newTotalNumTiles < this.tiles.length) {
+            // reset turret pos
+            this.turret.setPosition(TURRET_START_POS_X, TURRET_START_POS_Y);
+
+            // remove tiles
+            while (this.tiles.length > newTotalNumTiles) {
+                console.debug("Destroying");
+                this.tiles.pop()?.destroy();
             }
-            level.push(row);
+        } else if (newTotalNumTiles > this.tiles.length) {
+            // reset turret pos
+            this.turret.setPosition(TURRET_START_POS_X, TURRET_START_POS_Y);
+            
+            // add new tiles
+            while (this.tiles.length < newTotalNumTiles) {
+                console.debug("Creating");
+                const tile = this.add.sprite(0, 0, 'tile').setInteractive();
+                tile.input!.dropZone = true;
+                this.tiles.push(tile);
+            }
         }
-        console.log(level);
-        return level;
+
+        this.tiles = Phaser.Actions.GridAlign(this.tiles, {
+            width: this.numTilesX,
+            height: this.numTilesY,
+            cellWidth: TILE_SIZE,
+            cellHeight: TILE_SIZE,
+            x: xCoord,
+            y: yCoord
+        });
     }
 
     create() {
-        var turret = this.add.sprite(100, 100, 'Turret');
-        turret.setInteractive();
+        this.createTurret();
+        this.loadBlockly();
+
+        if (this.numTilesX == undefined)
+            this.numTilesX = INIT_TILES;
+        if (this.numTilesY == undefined)
+            this.numTilesY = INIT_TILES;
+
+        this.generateLevel();
+
+        this.setDragEvents();
+
+        // Add a collider to the map (used for several things, such as the player)
+        // map.setCollisionByExclusion([-1]);
+        document.getElementById("resize")?.addEventListener("click", e => {
+            // TODO: get x, y values from inside phaser
+            this.numTilesX = parseInt((<HTMLInputElement>document.getElementById("dimX")).value, 10);
+            this.numTilesY = parseInt((<HTMLInputElement>document.getElementById("dimY")).value, 10);
+
+            this.generateLevel();
+        });
+    }
+
+    setDragEvents(){
+        this.input.on('dragenter', (pointer, gameObject, dropZone) => {
+            dropZone.setTint(0x00ff00);
+        });
+
+        this.input.on('dragleave', (pointer, gameObject, dropZone) => {
+            dropZone.clearTint();
+        });
+
+        this.input.on('drop', (pointer, gameObject, dropZone) => {
+            gameObject.x = dropZone.x;
+            gameObject.y = dropZone.y;
+
+            dropZone.clearTint();
+        });
+    }
+
+    createTurret() {
+        this.turret = this.add.sprite(TURRET_START_POS_X, TURRET_START_POS_Y, 'Turret');
+        this.turret.setInteractive();
         const targetWidth = 200;
         const targetHeight = 200;
-        const scaleFactor = Math.min(targetWidth / turret.width, targetHeight / turret.height);
-        turret.setScale(scaleFactor);
+        const scaleFactor = Math.min(targetWidth / this.turret.width, targetHeight / this.turret.height);
+        this.turret.setScale(scaleFactor);
 
         // Enable drag behavior for the sprite
-        this.input.setDraggable(turret);
+        this.input.setDraggable(this.turret);
 
-        // Define what happens when the sprite is dragged
+        this.input.on('dragstart', function (pointer, gameObject) {
+            this.children.bringToTop(gameObject);
+        }, this);
+
         this.input.on('drag', (pointer, gameObject, dragX, dragY) => {
             gameObject.x = dragX;
             gameObject.y = dragY;
         });
-        this.loadBlockly();
-        // Load a map from a 2D array of tile indices
-        // const level = this.generateLevel(dimX, dimY, 0);
-        let level = this.generateLevel(INIT_TILES, INIT_TILES, 0);
-        // Varibles to center the tilemap
-        const SCREEN_X = this.cameras.main.worldView.x + this.cameras.main.width / 2;
-        const SCREEN_Y = this.cameras.main.worldView.y + this.cameras.main.height / 2;
-        // When loading from an array, make sure to specify the tileWidth and tileHeight
-        let map = this.make.tilemap({ data: level, tileWidth: TILE_WIDTH, tileHeight: TILE_HEIGHT });
-
-        let tiles = map.addTilesetImage('tileAsset')!;
-        let layer = map.createLayer(0, tiles, SCREEN_X - (INIT_TILES / 2 * TILE_WIDTH), SCREEN_Y - (INIT_TILES / 2 * TILE_HEIGHT))!;
-       // this.createDropZones(layer);
-        document.getElementById("resize")?.addEventListener("click", e => {
-
-            //get values from input fields into variables:
-            let dimX = parseInt((<HTMLInputElement>document.getElementById("dimX")).value, 10);
-            let dimY = parseInt((<HTMLInputElement>document.getElementById("dimY")).value, 10);
-            let tileValue = 0;
-            //if values are not empty, generate level with given values:
-            if (dimX > 0 && dimY > 0) {
-                level = this.generateLevel(dimX, dimY, tileValue);
-                map.destroy();
-
-                map = this.make.tilemap({ data: level, tileWidth: TILE_WIDTH, tileHeight: TILE_HEIGHT });
-                tiles = map.addTilesetImage('tileAsset')!;
-                layer = map.createLayer(0, tiles, SCREEN_X - (dimY / 2 * TILE_WIDTH), SCREEN_Y - (dimX / 2 * TILE_HEIGHT))!;
-                this.createDropZones(layer);
-            }
-        });
-    }
-
-
-    createDropZones(layer: Phaser.Tilemaps.TilemapLayer) {
-        this.dropZones = [];
-        layer.forEachTile((tile, indexX, indexY) => {
-            let zone = this.add.zone(tile.getCenterX(), tile.getCenterY(), TILE_WIDTH, TILE_HEIGHT).setOrigin(0.5);
-            zone.setData('x', indexX);
-            zone.setData('y', indexY);
-            zone.setData('tile', tile);
-            zone.setData('color', 0x00ffff);
-            zone.setData('border', this.add.rectangle(zone.x, zone.y, TILE_WIDTH, TILE_HEIGHT, zone.getData('color')).setOrigin(0));
-            if (!this.dropZones[indexX]) {
-                this.dropZones[indexX] = [];
-            }
-            this.dropZones[indexX][indexY] = zone;
-        });
-
     }
 
     loadBlockly() {
@@ -108,29 +140,32 @@ export default class LevelEditor extends Phaser.Scene {
         defineBlocks();
 
         this.workspace = Blockly.inject(this.blocklyDiv, { toolbox });
+        document.getElementById("play")?.addEventListener("click", e => this.log());
 
-        const localBlocklyDiv = this.blocklyDiv;
-        const localBlocklyArea = this.blocklyArea;
-        const localWorkspace = this.workspace;
+        window.addEventListener('resize', this.blocklyResize, false);
+        this.blocklyResize();
+    }
 
-        const onresize = function () {
-            // Compute the absolute coordinates and dimensions of blocklyArea.
-            let element = localBlocklyArea;
-            let x = 0;
-            let y = 0;
-            do {
-                x += element.offsetLeft;
-                y += element.offsetTop;
-                element = <HTMLElement>element.offsetParent;
-            } while (element);
-            // Position blocklyDiv over blocklyArea.
-            localBlocklyDiv.style.left = x + 'px';
-            localBlocklyDiv.style.top = y + 'px';
-            localBlocklyDiv.style.width = localBlocklyArea.offsetWidth + 'px';
-            localBlocklyDiv.style.height = localBlocklyArea.offsetHeight + 'px';
-            Blockly.svgResize(localWorkspace);
-        };
-        window.addEventListener('resize', onresize, false);
-        onresize(); 
+    blocklyResize() {
+        // Compute the absolute coordinates and dimensions of blocklyArea.
+        let element = this.blocklyArea;
+        let x = 0;
+        let y = 0;
+        do {
+            x += element.offsetLeft;
+            y += element.offsetTop;
+            element = <HTMLElement>element.offsetParent;
+        } while (element);
+        // Position blocklyDiv over blocklyArea.
+        this.blocklyDiv.style.left = x + 'px';
+        this.blocklyDiv.style.top = y + 'px';
+        this.blocklyDiv.style.width = this.blocklyArea.offsetWidth + 'px';
+        this.blocklyDiv.style.height = this.blocklyArea.offsetHeight + 'px';
+        Blockly.svgResize(this.workspace);
+    }
+
+    log() {
+        console.log(javascriptGenerator.workspaceToCode(this.workspace));
+        console.log("here");
     }
 }
