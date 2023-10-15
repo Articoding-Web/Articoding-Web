abe4f5ae1db218cfbc50309fa545e17ca30fb1bd
nico <nico.mooser@gmail.com>
2023-10-15 14:19:20 +0200

    Fix: delete objs

--- src/Phaser/Classes/ArticodingObject.ts
+++ src/Phaser/Classes/ArticodingObject.ts
@@ -1,103 +1,112 @@
 import * as Phaser from "phaser";
+import TileObject from "./TileObject";
 
 export default class ArticodingObject extends Phaser.GameObjects.Sprite {
-  allowMultiple: Boolean; // allow object to be duplicated or not
-  frameString: string | number | undefined;
-  onDropZone: Boolean = false;
-  origX: number;
-  origY: number;
-  scene: Phaser.Scene;
-  constructor(
-    scene: Phaser.Scene,
-    x: number,
-    y: number,
-    texture: string | Phaser.Textures.Texture,
-    allowMultiple: Boolean,
-    frame?: string | number | undefined
-  ) {
-    super(scene, x, y, texture, frame);
-    this.scene = scene;
-    this.frameString = frame;
-    this.allowMultiple = allowMultiple;
-    this.origX = x;
-    this.origY = y;
+    allowMultiple: Boolean; // allow object to be duplicated or not
+    allowDestruction: Boolean = false;
+    frameString: string | number | undefined;
+    isOnDropZone: Boolean = false;
+    origX: number;
+    origY: number;
+    dropZone: TileObject | undefined;
 
-    const scaleFactor = Math.min(100 / this.width, 100 / this.height); //TODO change texture
-    this.setScale(scaleFactor);
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string | Phaser.Textures.Texture, allowMultiple: Boolean, frame?: string | number | undefined, allowDestruction?: Boolean) {
+        super(scene, x, y, texture, frame);
+        this.scene = scene;
+        this.frameString = frame;
+        this.allowMultiple = allowMultiple;
+        this.origX = x;
+        this.origY = y;
+        this.allowDestruction = allowDestruction;
 
-    this.setInteractive();
-    this.scene.input.setDraggable(this);
+        const scaleFactor = Math.min(100 / this.width, 100 / this.height); //TODO change texture
+        this.setScale(scaleFactor);
 
-    this.on("drag", (pointer, dragX, dragY) => this.onDrag(dragX, dragY));
-    this.on("dragstart", () => this.onDragStart());
-    this.on("dragenter", (pointer, dropZone) =>
-      this.onDragEnter(pointer, dropZone)
-    );
-    this.on("dragleave", (pointer, dropZone) =>
-      this.onDragLeave(pointer, dropZone)
-    );
-    this.on("dragend", (pointer) => this.onDragEnd(this.x, this.y));
-    this.on("drop", (pointer, dropZone) => this.onDrop(dropZone));
+        this.setInteractive();
+        this.scene.input.setDraggable(this);
 
-    this.scene.add.existing(this);
-  }
+        this.on("drag", (pointer, dragX, dragY) => this.onDrag(dragX, dragY));
+        this.on("dragstart", () => this.onDragStart());
+        this.on("dragenter", (pointer, dropZone) => this.onDragEnter());
+        this.on("dragleave", (pointer, dropZone) => this.onDragLeave());
+        this.on("dragend", (pointer) => this.onDragEnd(this.x, this.y));
+        this.on("drop", (pointer, dropZone) => this.onDrop(dropZone));
 
-  onDrag(dragX, dragY) {
-    this.x = dragX;
-    this.y = dragY;
-  }
-
-  onDragStart() {
-    this.scene.children.bringToTop(this);
-  }
-
-  onDragEnter(pointer, dropZone) {
-    dropZone.setTint(0x00ff00);
-    this.onDropZone = true;
-  }
-
-  onDragLeave(pointer, dropZone) {
-    dropZone.clearTint();
-    console.log(dropZone.gameObject);
-    this.onDropZone = false;
-    dropZone.occupied = false;
-  }
-
-  onDragEnd(x: number, y: number) {
-      if(!this.onDropZone){
-        this.x = this.origX;
-        this.y = this.origY;
-        if (!this.allowMultiple) this.destroy();
-      }
-  }
-
-  onDrop(dropZone) {
-    if (this.allowMultiple) {
-      this.x = this.origX;
-      this.y = this.origY;
-      if(this.onDropZone && !dropZone.occupied){
-        new ArticodingObject(
-          this.scene,
-          dropZone.x,
-          dropZone.y,
-          this.texture,
-          false,
-          this.frameString
-        );
-        dropZone.occupied = true;
-      }
-    } else {
-      if(!dropZone.occupied){
-        this.x = dropZone.x;
-        this.y = dropZone.y;
-        this.origX = this.x;
-        this.origY = this.y;
-        dropZone.occupied = true;
-      }
-      else{
-        this.x = this.origX;
-        this.y = this.origY;
-      }
+        this.scene.add.existing(this);
     }
-  }
+
+    onDrag(dragX : number, dragY : number) {
+        this.x = dragX;
+        this.y = dragY;
+    }
+
+    onDragStart() {
+        this.scene.children.bringToTop(this);
+    }
+
+    onDragEnter() {
+        this.isOnDropZone = true;
+    }
+
+    onDragLeave() {
+        this.isOnDropZone = false;
+    }
+
+    onDragEnd(x: number, y: number) {
+        if (!this.isOnDropZone) {
+            this.resetDropZone();
+
+            // Reset/Destroy object
+            if(this.allowDestruction){
+                this.destroy();
+            } else {
+                this.x = this.origX;
+                this.y = this.origY;
+            }
+        }
+    }
+
+    onDrop(dropZone : TileObject) {
+        if (this.isOnDropZone && !dropZone.occupied) {
+            this.resetDropZone();
+
+            if (this.allowMultiple) {
+                // Duplicate object as deletable
+                let newObj = new ArticodingObject(
+                    this.scene,
+                    dropZone.x,
+                    dropZone.y,
+                    this.texture,
+                    false,
+                    this.frameString,
+                    true
+                );
+                newObj.dropZone = dropZone;
+
+                // Reset position
+                this.x = this.origX;
+                this.y = this.origY;
+                this.isOnDropZone = false;
+            } else {
+                // Set position
+                this.x = dropZone.x;
+                this.y = dropZone.y;
+                this.dropZone = dropZone;
+            }
+
+            // Zone occupied
+            dropZone.occupied = true;
+        } else {
+            this.x = this.origX;
+            this.y = this.origY;
+        }
+    }
+
+    resetDropZone(){
+        console.log(this.dropZone);
+        if(this.dropZone !== undefined){
+            this.dropZone.occupied = false;
+            this.dropZone = undefined;
+        }
+    }
 }
--- src/Phaser/Classes/TileObject.ts
+++ src/Phaser/Classes/TileObject.ts
@@ -1,20 +1,15 @@
 import * as Phaser from "phaser";
 
 export default class TileObject extends Phaser.GameObjects.Sprite {
-    occupied: Boolean; 
-    origX: number;
-    origY: number;
-    scene: Phaser.Scene;
+    occupied: Boolean = false;
 
     constructor( scene: Phaser.Scene, x: number, y: number,
-        texture: string | Phaser.Textures.Texture, occupied: Boolean){
+        texture: string | Phaser.Textures.Texture){
         super(scene, x, y, texture);
         this.scene = scene;
-        this.occupied = occupied;
 
         this.setInteractive();
         this.input!.dropZone = true;
         this.scene.add.existing(this);
     }
-
 }
