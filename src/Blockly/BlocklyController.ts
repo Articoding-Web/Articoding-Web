ea4b852a68e9019684a31043836eec7f920cbd73
Santiago Moral Santorio <santimoralsantorio@gmail.com>
2023-11-08 11:29:54 +0100

    toolbox se carga excluyendo los bloques o categorias que le digamos. TODO: tenemos que mergear esto y hacer rebase de esta rama

--- src/Blockly/BlocklyController.ts
+++ src/Blockly/BlocklyController.ts
@@ -13,8 +13,9 @@ export default class BlocklyController {
   startBlock : Blockly.BlockSvg;
   workspace: Blockly.WorkspaceSvg;
 
-  constructor() {
-    this.workspace = Blockly.inject(this.blocklyDiv, { toolbox });
+  constructor(removeBlocks : string[],removeCategories: string[]) {
+    
+    this.loadToolbox(removeBlocks,removeCategories);
     globalThis.workspace = this.workspace;
     Blockly.defineBlocksWithJsonArray(blocks);
     this.startBlock = this.workspace.newBlock('start');
@@ -24,7 +25,25 @@ export default class BlocklyController {
     this.startBlock.moveBy(10000, 100);
     block_code.defineAllBlocks();
   }
+  //vamos a especificar que bloques se pueden usar en el workspace
+  loadToolbox(removedBlocks: string[], removedCategories: string[]) {
+    if (removedBlocks === undefined || removedCategories === undefined) {
+      return; //esto no va a hacer falta.
+    }
+    let toolboxClone = JSON.parse(JSON.stringify(toolbox));
 
+    toolboxClone.contents = toolboxClone.contents.filter(category => {
+        if (removedCategories.indexOf(category.name) !== -1) { return false;}
+        category.contents = category.contents.filter(block => {
+            if (removedBlocks.indexOf(block.type) !== -1) {return false;}
+            return true;
+        });
+        if (category.contents.length > 0) {return true;}
+        return false;
+    });
+    //cargamos un toolbox especifico
+    this.workspace = Blockly.inject(this.blocklyDiv,toolboxClone);
+}
   showWorkspace() {
     globalThis.blocklyArea.classList.remove("d-none");
     this.isVisible = true;
