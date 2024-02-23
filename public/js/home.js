67e10a185985a4236e534a6c1a2596a87607f25c
Nicolas Espinosa Mooser <nico.mooser@gmail.com>
2024-02-23 18:43:34 +0100

    Refactor para reutilizar funciones y jsdoc

--- public/js/home.js
+++ public/js/home.js
@@ -1,67 +1,13 @@
+"use strict"
+
 const API_ENDPOINT = "http://localhost:3001/api";
 
-let content = null;
-let categories = [];
-
-const categoriesView = async (categories) => {
-  let view = "";
-  for (const category of categories) {
-    category.levels = await fetchRequest(
-      `${API_ENDPOINT}/level/countByCategory/${category.id}`,
-      "GET"
-    );
-    view += categoryDiv(category);
-  }
-  return view;
-};
-
-const categoryDiv = (category) => {
-  let view = `
-  <div class="col">
-    <a class="category" href="${API_ENDPOINT}/level/levelsByCategory/${category.id}">
-      <div class="card border-dark d-flex flex-column h-100">
-          <h5 class="card-header card-title text-dark">
-            ${category.name}
-          </h5>
-          <div class="card-body text-dark">
-            <h6 class="card-subtitle mb-2 text-muted">
-              Niveles: ${category.levels}
-            </h6>
-            ${category.description}
-          </div>
-      </div>
-    </a>
-  </div>
-  `;
-  return view;
-};
-
-const categoryLevelsView = async (category) => {
-  let view = "";
-  for (const level of category) {
-    view += categoryLevelsDiv(level);
-  }
-  return view;
-};
-
-const categoryLevelsDiv = (level) => {
-  let view = `
-  <div class="col">
-    <a class="getLevel" href="${API_ENDPOINT}/level/${level.id}">
-      <div class="card border-dark">
-        <div class="card-header">
-          ${level.title}
-        </div>
-        <div class="card-body">
-          Miniatura: 
-        </div>
-      </div>
-    </a>
-  </div>
-  `;
-  return view;
-};
-
+/**
+ * 
+ * @param {URL} endpoint endpoint where the call should be made
+ * @param {String} method "GET" is the only method supported 
+ * @returns json response
+ */
 async function fetchRequest(endpoint, method) {
   const response = fetch(endpoint, {
     method,
@@ -72,54 +18,17 @@ async function fetchRequest(endpoint, method) {
   return (await response).json();
 }
 
-const initController = async () => {
-  content = document.getElementById("categories");
-  categories = await fetchRequest(`${API_ENDPOINT}/level/categories`, "GET");
-  content.innerHTML = await categoriesView(categories);
-  document.querySelectorAll("a.category").forEach((anchorTag) => {
-    anchorTag.addEventListener("click", loadCategory);
-  });
-  const community = document.getElementById("community");
-  community.addEventListener("click", printCommunityLevels);
-};
-
-async function printCommunityLevels(event) {
-  event.preventDefault();
-  const communityLevels = await fetchRequest(
-    `${API_ENDPOINT}/level/community/levels`,
-    "GET"
-  );
-  console.log("Niveles:", communityLevels);
-  content.innerHTML = await buildCommunity(communityLevels);
-}
-
-async function buildCommunity(levels) {
-  let view = "";
-  for (const level of levels) {
-    view += categoryLevelsDiv(level);
+/**
+ * 
+ * @param {HTMLDivElement} divElement where the items generated are appended 
+ * @param {Array} items items used to generate html that is inserted
+ * @param {Function} htmlGenerator html string generator to process items and generate html
+ */
+async function fillContent(divElement, items, htmlGenerator) {
+  divElement.innerHTML = '';
+  for (let item of items) {
+    divElement.insertAdjacentHTML("beforeend", await htmlGenerator(item));
   }
-  return view;
-}
-
-async function loadCategory(event) {
-  event.preventDefault();
-  const anchorTag = event.target.closest("a.category");
-
-  const id = anchorTag.href.split("/level/levelsByCategory/")[1];
-  const levels = await fetchRequest(
-    `${API_ENDPOINT}/level/levelsByCategory/${id}`,
-    "GET"
-  );
-
-  content = document.getElementById("categories");
-  content.innerHTML = await categoryLevelsView(levels);
-
-  // Add getLevel event listener
-  const getLevelanchorTags = document
-    .querySelectorAll("a.getLevel")
-    .forEach((level) => {
-      level.addEventListener("click", playLevel);
-    });
 }
 
 function playLevel(event) {
@@ -129,8 +38,126 @@ function playLevel(event) {
   const levelId = anchorTag.href.split("level/")[1];
 }
 
-const init = () => {
-  document.addEventListener("DOMContentLoaded", initController);
-};
+/**
+ * Sets click listeners for navbar items
+ */
+async function setNavbarListeners() {
+  // Official Levels
+  document.getElementById("official").addEventListener("click", loadCategories);
 
-init();
+  // TODO: Manual
+
+  // Community Levels
+  document.getElementById("community").addEventListener("click", loadCommunityLevels);
+}
+
+/**
+ * 
+ * @param {Object} category category with id, name, levels and description
+ * @returns String of HTMLDivElement
+ */
+async function generateCategoryDiv(category) {
+  category.levels = await fetchRequest(`${API_ENDPOINT}/level/countByCategory/${category.id}`, "GET");
+  return `<div class="col">
+            <a class="category" href="${API_ENDPOINT}/level/levelsByCategory/${category.id}">
+              <div class="card border-dark d-flex flex-column h-100">
+                  <h5 class="card-header card-title text-dark">
+                    ${category.name}
+                  </h5>
+                  <div class="card-body text-dark">
+                    <h6 class="card-subtitle mb-2 text-muted">
+                      Niveles: ${category.levels}
+                    </h6>
+                    ${category.description}
+                  </div>
+              </div>
+            </a>
+          </div>`;
+}
+
+/**
+ * Gets categories from DB and shows them on screen
+ */
+async function loadCategories() {
+  const divElement = document.getElementById("categories");
+  const categories = await fetchRequest(`${API_ENDPOINT}/level/categories`, "GET");
+
+  await fillContent(divElement, categories, generateCategoryDiv);
+
+  document.querySelectorAll("a.category").forEach((anchorTag) => {
+    anchorTag.addEventListener("click", loadCategoryLevels);
+  });
+}
+
+/**
+ * 
+ * @param {Object} level with id, title, etc
+ * @returns String of HTMLDivElement
+ */
+async function generateLevelDiv(level) {
+  return `<div class="col">
+            <a class="getLevel" href="${API_ENDPOINT}/level/${level.id}">
+              <div class="card border-dark">
+                <div class="card-header">
+                  ${level.title}
+                </div>
+                <div class="card-body">
+                  Miniatura: 
+                </div>
+              </div>
+            </a>
+          </div>`;
+}
+
+/**
+ * 
+ * @param {Event} event
+ * Gets levels of a category from DB and shows them on screen
+ */
+async function loadCategoryLevels(event) {
+  event.preventDefault();
+  const anchorTag = event.target.closest("a.category");
+
+  const id = anchorTag.href.split("/level/levelsByCategory/")[1];
+  const levels = await fetchRequest(`${API_ENDPOINT}/level/levelsByCategory/${id}`, "GET");
+
+  const divElement = document.getElementById("categories");
+
+  await fillContent(divElement, levels, generateLevelDiv);
+
+  // Add getLevel event listener
+  document.querySelectorAll("a.getLevel").forEach((level) => {
+    level.addEventListener("click", playLevel);
+  });
+}
+
+/**
+ * 
+ * @param {Event} event 
+ * Gets community levels from DB and shows them on screen
+ */
+async function loadCommunityLevels() {
+  const divElement = document.getElementById("categories");
+
+  const communityLevels = await fetchRequest(
+    `${API_ENDPOINT}/level/community/levels`,
+    "GET"
+  );
+
+  await fillContent(divElement, communityLevels, generateLevelDiv);
+
+  document.querySelectorAll("a.getLevel").forEach((level) => {
+    level.addEventListener("click", playLevel);
+  });
+}
+
+/**
+ * init function
+ */
+(async function () {
+  // Create row
+  document.getElementById("content").innerHTML = '<div class="row row-cols-1 row-cols-md-3 row-cols-lg-4 g-2" id="categories"></div>';
+
+  setNavbarListeners();
+  loadCategories();
+})();
