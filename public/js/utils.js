bf98e87f4ffa993196e036c9b317d89a7300a3f4
Santiago Moral Santorio <santimoralsantorio@gmail.com>
2024-03-24 09:42:43 +0100

    POR FIN se restartea el nivel sin borrar blockly, pero tema rana muerta/viva explota (TODO)

--- public/js/utils.js
+++ public/js/utils.js
@@ -8,13 +8,14 @@ let defeatModalInstance;
     nextLevelButton.addEventListener("click", () => {
         let contentElement = document.getElementById("content");
         let levelId = parseInt(contentElement.getAttribute("data-level-id"));
+        let currentLevelId = levelId;
         levelId += 1;
         startLevelById(levelId);//TODO refactorizar para que si no lo carga, (porque es el ultimo nivel de la categoria, pase de categoria o algo)
         document.getElementById("content").setAttribute("data-level-id", levelId);
     });
 
     const retryLevelButton = document.querySelector("#defeatModal .btn-primary");
-    retryLevelButton.addEventListener("click", () => restartCurrentLevel());
+    retryLevelButton.addEventListener("click", () => restartCurrentLevel(currentLevelId));
 })();
 
 // Listen for the "winConditionModal" event
--- src/client.ts
+++ src/client.ts
@@ -2,11 +2,8 @@ import PhaserController from "./Phaser/PhaserController";
 import BlocklyController from "./Blockly/BlocklyController";
 import LevelPlayer from "./Phaser/scenes/LevelPlayer";
 import LevelEditor from "./Phaser/scenes/LevelEditor";
-
 // Temp
 import level from './baseLevel.json';
-
-
 const BLOCKLY_DIV_ID = "blocklyDiv";
 
 let blocklyController: BlocklyController;
@@ -24,6 +21,49 @@ export function startLevel(level) {
   blocklyController = new BlocklyController(BLOCKLY_DIV_ID, toolbox, maxInstances, workspaceBlocks);
 }
 
+//esta tiene que ser asincrona para poder hacer el fetch
+export async function startLevelById(levelId: number) {
+  try {
+    const response = await fetch(`http://localhost:3001/api/level/${levelId}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const level = await response.json();
+    //TODO cambiarlo para que blockly salga tal y como estaba si has fracasado el nivel
+    blocklyController.destroy();
+    phaserController.destroy();
+    startLevel(level);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+}
+
+export async function restartCurrentLevel(levelId: number) {
+  try {
+    const response = await fetch(`http://localhost:3001/api/level/${levelId}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const level = await response.json();
+    let blocklyControllerRef = blocklyController;
+    blocklyController.destroy();//TODO no destruir blockly... mantenerlo igual.
+    phaserController.destroy();
+    const levelJSON = JSON.parse(level.data);
+
+    const toolbox = levelJSON.blockly.toolbox;
+    const maxInstances = levelJSON.blockly.maxInstances;
+    const workspaceBlocks = levelJSON.blockly.workspaceBlocks;
+    const phaserJSON = levelJSON.phaser;
+
+    phaserController = new PhaserController("LevelPlayer", LevelPlayer, phaserJSON);
+    blocklyController = blocklyControllerRef;
+    //blocklyController se mantiene en workspace.
+    //blocklyController = new BlocklyController(BLOCKLY_DIV_ID, toolbox, maxInstances, workspaceBlocks);
+  } catch (error) {
+    console.error('Error:', error);
+  }
+
+}
 export function editLevel() {
   phaserController = new PhaserController("LevelEditor", LevelEditor);
 }
