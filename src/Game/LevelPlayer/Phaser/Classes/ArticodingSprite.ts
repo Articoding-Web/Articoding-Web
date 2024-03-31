d919e9e4e6fd6ab79661ff4f4fd54296ebbb59f9
Nicolas Espinosa Mooser <nico.mooser@gmail.com>
2024-03-31 20:42:14 +0200

    Add text background

--- src/Game/LevelPlayer/Phaser/Classes/ArticodingSprite.ts
+++ src/Game/LevelPlayer/Phaser/Classes/ArticodingSprite.ts
@@ -1,5 +1,6 @@
 import * as Phaser from "phaser";
 import { Player } from "./Player";
+import config from "../../../config";
 
 export default abstract class ArticodingSprite extends Phaser.GameObjects.Sprite {
   private idText: Phaser.GameObjects.Text;
@@ -9,22 +10,22 @@ export default abstract class ArticodingSprite extends Phaser.GameObjects.Sprite
     scene.add.existing(this);
 
     // Add id
-    if(id) {
-      this.idText = this.scene.add.text(100, 100, id);
+    if (id) {
+      this.idText = this.scene.add.text(100, 100, id, { backgroundColor: "#6c757d" });
     }
   }
 
-  getPosition() : Phaser.Math.Vector2 {
+  getPosition(): Phaser.Math.Vector2 {
     return new Phaser.Math.Vector2(this.tileX, this.tileY);
   }
 
   scaleIdText() {
-    if(this.idText) {
-      this.idText.setPosition(this.x, this.y);
+    if (this.idText) {
+      this.idText.setPosition(this.x, this.y - config.TILE_SIZE / 10 * this.scale);
       this.idText.setDepth(10);
       this.idText.setOrigin(0.5, 0);
     }
   }
-  
+
   abstract collide(player: Player): void;
 }
