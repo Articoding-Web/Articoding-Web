fba2ef1eb42f059033804ace719dc9c72e7b4fb9
Santiago Moral Santorio <santimoralsantorio@gmail.com>
2024-04-04 15:41:53 +0200

    stage

--- src/Game/config.ts
+++ src/Game/config.ts
@@ -1,4 +1,7 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const config = {
     TILE_SIZE: 16,  // in pixels
     MOVEMENT_ANIMDURATION: 900, // in milliseconds
@@ -7,6 +10,6 @@ const config = {
     EDITOR_MIN_COLS: 3,
     EDITOR_MAX_ROWS: 8,
     EDITOR_MAX_COLS: 8,
-}
+};
 
 export default config;
