02cb6c959bae4010848c0d8fa1af84c1ec3a763d
Santiago Moral Santorio <santimoralsantorio@gmail.com>
2024-04-18 16:59:00 +0200

    listo, falta ver si tambien se puede aun cuando stylesheet se niega a cargar por nonce cors de seguridad..

--- src/SPA/loaders/levelPlayerLoader.ts
+++ src/SPA/loaders/levelPlayerLoader.ts
@@ -50,11 +50,15 @@ function getEditButton(fromLevelEditor: boolean) {
  */
 export default async function playLevelById(id: string) {
     let level = await fetchRequest(`${API_ENDPOINT}/level/${id}`, "GET");
-    loadLevel(JSON.parse(level.data));
+    loadLevel(JSON.parse(level.data),false,level.category);
 }
 
-export async function loadLevel(levelJSON: Level.Level, fromLevelEditor?: boolean) {
+export async function loadLevel(levelJSON: Level.Level, fromLevelEditor?: boolean, category?: string) {
+    if(category){
+        document.getElementById("content").setAttribute("categoryIndex",category);
+    }
     document.getElementById("content").innerHTML = getLevelPlayerHTML(fromLevelEditor);
+    //document.getElementById("content").setAttribute("categoryIndex",)
     currentLevelJSON = levelJSON;
 
     const toolbox = levelJSON.blockly.toolbox;
